dcff63d9f560406fda0bd97dcd2cfa2adc0b0c95
Sitam <sitammeur@gmail.com>
2024-07-14 01:18:42 +0530

    update app file

--- app.py
+++ app.py
@@ -1,34 +1,6 @@
 # Importing the requirements
 import gradio as gr
-from transformers import BlipProcessor, BlipForQuestionAnswering
-
-# Load the model and processor
-processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
-model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
-
-
-# Function to answer the question
-def answer_question(image, text):
-    """
-    Generates an answer to a given question based on the provided image and text.
-
-    Args:
-        image (str): The path to the image file.
-        text (str): The question text.
-
-    Returns:
-        str: The generated answer to the question.
-    """
-
-    # Process the inputs and generate the ids
-    inputs = processor(images=image, text=text, return_tensors="pt")
-    generated_ids = model.generate(**inputs, max_length=50)
-
-    # Decode the generated IDs
-    generated_answer = processor.batch_decode(generated_ids, skip_special_tokens=True)
-
-    # Return the generated answer
-    return generated_answer[0]
+from model import answer_question
 
 
 # Image and text inputs for the interface
@@ -40,9 +12,9 @@ answer = gr.Textbox(label="Predicted answer")
 
 # Examples for the interface
 examples = [
-    ["cat.jpg", "How many cats are there?"],
-    ["dog.jpg", "What color is the dog?"],
-    ["bird.jpg", "What is the bird doing?"],
+    ["images/cat.jpg", "How many cats are there?"],
+    ["images/dog.jpg", "What color is the dog?"],
+    ["images/bird.jpg", "What is the bird doing?"],
 ]
 
 # Title, description, and article for the interface
